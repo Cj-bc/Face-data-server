cb1e2e870741d731c3d10325892c415de5fb9342
Cj-bc <cj-bc@y-modify.org>
2019-06-30 02:38:52 +0900

    Add: face-detect> face detection codes and datas; THIS IS NOT WRITTEN BY ME
    I use [this site's codes](https://qiita.com/kekeho/items/0b2d4ed5192a4c90a0ac) to generate .dat,
    and find.py.
    Thanks to @kekeho

---
+++ face_detection/find.py
@@ -0,0 +1,80 @@
+# THIS CODE IS NOT WRITTEN BY ME, BUT BY @kekeho
+# Refer to: https://qiita.com/kekeho/items/0b2d4ed5192a4c90a0ac
+#
+import cv2
+import dlib
+import numpy
+import os
+
+# Cascade files directory path
+CASCADE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/haarcascades/"
+LEARNED_MODEL_PATH = os.path.dirname(
+    os.path.abspath(__file__)) + "/learned-models/"
+predictor = dlib.shape_predictor(
+    LEARNED_MODEL_PATH + 'helen-dataset.dat')
+face_cascade = cv2.CascadeClassifier(
+    CASCADE_PATH + 'haarcascade_frontalface_default.xml')
+
+
+def face_position(gray_img):
+    """Detect faces position
+    Return:
+        faces: faces position list (x, y, w, h)
+    """
+    faces = face_cascade.detectMultiScale(gray_img, minSize=(100, 100))
+    return faces
+
+
+def facemark(gray_img):
+    """Recoginize face landmark position by i-bug 300-w dataset
+    Return:
+        randmarks = [
+        [x, y],
+        [x, y],
+        ...
+        ]
+        [0~40]: chin
+        [41~57]: nose
+        [58~85]: outside of lips
+        [86-113]: inside of lips
+        [114-133]: right eye
+        [134-153]: left eye
+        [154-173]: right eyebrows
+        [174-193]: left eyebrows
+    """
+    faces_roi = face_position(gray_img)
+    landmarks = []
+
+    for face in faces_roi:
+        x, y, w, h = face
+        face_img = gray_img[y: y + h, x: x + w];
+
+        detector = dlib.get_frontal_face_detector()
+        rects = detector(gray_img, 1)
+
+        landmarks = []
+        for rect in rects:
+            landmarks.append(
+                numpy.array(
+                    [[p.x, p.y] for p in predictor(gray_img, rect).parts()])
+            )
+    return landmarks
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        landmarks = facemark(gray)
+
+        for landmark in landmarks:
+            for points in landmark:
+                cv2.drawMarker(frame, (points[0], points[1]), (21, 255, 12))
+        cv2.imshow("video frame", frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
