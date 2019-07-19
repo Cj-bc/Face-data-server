775c18824c309a2b561b7e885962ac58656d497d
Cj-bc <cj-bc@y-modify.org>
2019-07-19 22:30:18 +0900

    Change: test/faceDetection> add type annotation

--- face_detection/tests/test_faceDetection.py
+++ face_detection/tests/test_faceDetection.py
@@ -6,6 +6,6 @@ import numpy
 
 
 @pytest.mark.parametrize("faceNum,expected", [(0, False), (1, True)])
-def test_isFaceExist(faceNum, expected):
+def test_isFaceExist(faceNum: int, expected: bool):
     with mock.patch('faceDetection.detector', dlib.rectangles(faceNum)):
         assert isFaceExist(numpy.ndarray(0)) == expected
