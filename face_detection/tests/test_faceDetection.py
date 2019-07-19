740e0d57e6af5f9c3ecffe20b7652db1cb0e6d08
Cj-bc <cj-bc@y-modify.org>
2019-07-19 22:28:47 +0900

    Change: tests/faceDetection> remove redundant keyword

--- face_detection/tests/test_faceDetection.py
+++ face_detection/tests/test_faceDetection.py
@@ -7,5 +7,5 @@ import numpy
 
 @pytest.mark.parametrize("faceNum,expected", [(0, False), (1, True)])
 def test_isFaceExist(faceNum, expected):
-    with mock.patch('faceDetection.detector', return_value=dlib.rectangles(faceNum)):
+    with mock.patch('faceDetection.detector', dlib.rectangles(faceNum)):
         assert isFaceExist(numpy.ndarray(0)) == expected
