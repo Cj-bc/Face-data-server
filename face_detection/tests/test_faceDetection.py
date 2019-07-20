2cce0273998d11302a597b6035b00371aa9463c4
Cj-bc <cj-bc@y-modify.org>
2019-07-20 13:06:46 +0900

    Fix: tests/faceDetection> revert commit '740e0d5'; it wasn't redundant

--- face_detection/tests/test_faceDetection.py
+++ face_detection/tests/test_faceDetection.py
@@ -7,5 +7,6 @@ import numpy
 
 @pytest.mark.parametrize("faceNum,expected", [(0, False), (1, True)])
 def test_isFaceExist(faceNum: int, expected: bool):
-    with mock.patch('faceDetection.detector', dlib.rectangles(faceNum)):
+    with mock.patch('faceDetection.detector',
+                    return_value=dlib.rectangles(faceNum)):
         assert isFaceExist(numpy.ndarray(0)) == expected
