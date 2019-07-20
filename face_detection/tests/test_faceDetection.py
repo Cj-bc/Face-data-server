cdd26d05afb2bfb1a09c88c7debfcd49b63277fa
Cj-bc <cj-bc@y-modify.org>
2019-07-20 13:07:32 +0900

    Add: tests/faceDetection> tests for 'getBiggestFace'

--- face_detection/tests/test_faceDetection.py
+++ face_detection/tests/test_faceDetection.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest import mock
-from faceDetection import isFaceExist
+from faceDetection import isFaceExist, getBiggestFace
 import dlib
 import numpy
 
@@ -10,3 +10,17 @@ def test_isFaceExist(faceNum: int, expected: bool):
     with mock.patch('faceDetection.detector',
                     return_value=dlib.rectangles(faceNum)):
         assert isFaceExist(numpy.ndarray(0)) == expected
+
+
+def test_getBiggestFace():
+    emptyPoints = dlib.points(41)
+
+    biggest = dlib.points(40)
+    biggest.append(dlib.point(100, 100))
+    faces = ([emptyPoints] * 5) + [biggest]
+
+    assert getBiggestFace(faces) == biggest
+
+
+def test_getBiggestFace_noface():
+    assert getBiggestFace([]) == dlib.points(41)
