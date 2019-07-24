c0dd10281a31cb391f897896d19f494421bad976
Cj-bc <cj-bc@y-modify.org>
2019-07-25 01:31:30 +0900

    Change: move 'main' codes to 'main.py'

--- faceDetection/find.py
+++ faceDetection/find.py
@@ -86,33 +86,3 @@ def rotates(landmark: dlib.points, calib: RawFaceData) -> FaceRotations:
     # v Is this correct code? v
     rotateZ = degreeZ
     return FaceRotations(rotateX, rotateY, rotateZ)
-
-
-def main():
-    print("connecting to camera...")
-    cap: cv2.VideoCapture = cv2.VideoCapture(0)
-    print("camera connected.")
-
-    try:
-        calibrated: RawFaceData = faceCalibration(cap)
-    except FaceDetectionError as e:
-        print(f"ERROR: Unexpected things are happened: {e}")
-        print("Aborting")
-        sys.exit(1)
-
-    while cap.isOpened():
-        rots: FaceRotations = FaceRotations(0, 0, 0)
-        _, frame = cap.read()
-        landmark: Optional[dlib.points] = facemark(frame)
-        print(f"landmark: {landmark}")  # DEBUG
-
-        if landmark is not None:
-            rots: FaceRotations = rotates(landmark, calibrated)
-
-        print(f"{datetime.datetime.today()}: {rots.x}, {rots.y}, {rots.z}")
-
-    cap.release()
-
-
-if __name__ == '__main__':
-    main()
