a2f93a09c76f99319d8f801c49147ccd6daefb5c
Cj-bc <cj-bc@y-modify.org>
2019-07-26 23:56:58 +0900

    Fix: test/Types> remove unused codes that caused error

--- tests/test_Types.py
+++ tests/test_Types.py
@@ -13,7 +13,7 @@ def test_RawFaceData_thresholded_noaffect(eD, fH, fC):
     assert target.thresholded(RawFaceData(eD, fH, fC)) == target
 
 
-def test_RawFaceData_thresholded_affect(eD, fH, fC):
+def test_RawFaceData_thresholded_affect():
     target = RawFaceData(1, 1, dlib.point(0, 0))
     assert target.thresholded(RawFaceData(0, 0, dlib.point(1, 1))) == \
             RawFaceData(0, 0, target.faceCenter)
