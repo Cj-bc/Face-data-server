8c4a0acdc7ad96229d6d7506ca153650b2735fc6
Cj-bc <cj-bc@y-modify.org>
2019-07-26 09:11:00 +0900

    Add: tests/Types> test for RawFaceData.threshold

---
+++ tests/test_Types.py
@@ -0,0 +1,19 @@
+import pytest
+import dlib
+
+from faceDetection.Types import (RawFaceData)
+
+
+@pytest.mark.parametrize('eD,fH,fC', [(2, 2, dlib.point(0, 0)),
+                                      (1, 1, dlib.point(0, 0)),
+                                      (1, 1, dlib.point(-1, -1))
+                                      ])
+def test_RawFaceData_thresholded_noaffect(eD, fH, fC):
+    target = RawFaceData(1, 1, dlib.point(0, 0))
+    assert target.thresholded(RawFaceData(eD, fH, fC)) == target
+
+
+def test_RawFaceData_thresholded_affect(eD, fH, fC):
+    target = RawFaceData(1, 1, dlib.point(0, 0))
+    assert target.thresholded(RawFaceData(0, 0, dlib.point(1, 1))) == \
+            RawFaceData(0, 0, target.faceCenter)
