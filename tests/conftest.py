9a0526fe6919c4429e722d5415511270ffc145f1
Cj-bc <cj-bc@y-modify.org>
2019-08-14 23:15:55 +0900

    Fix: tests/conftest> limit lenght of float to 32 bit
    This would prevent errors like
    
    ```
    >       assert (a + b) + c == a + (b + c)
    E       assert Coord(0.0, 8192.000000000007) == Coord(0.0,
    8192.000000000005)
    E         -Coord(0.0, 8192.000000000007)
    E         +Coord(0.0, 8192.000000000005)
    ```
    
    Which was occurred due to floating point calculation problem

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ noFaceFrame: Cv2Image = cv2.imread('tests/src/noface.png')
 
 
 finiteFloatCallable = st.floats(allow_infinity=False, allow_nan=False
-                               , min_value=0.0, max_value=1.0e4)
+                               , min_value=0.0, max_value=1.0e4, width=32)
 
 CoordStrategies = st.builds(Coord, finiteFloatCallable, finiteFloatCallable)
 PartStrategies = st.builds(Part, CoordStrategies, CoordStrategies
