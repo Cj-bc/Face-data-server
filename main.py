f29354693891a0120e147d06102b7ddcbbbd57c4
Cj-bc <cj.bc-sd@outlook.jp>
2020-03-02 22:03:30 +0900

    Change: main> draw face wire for debugging

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from contextlib import closing
 import secrets
 import socket
 from concurrent import futures
-from typing import (Optional, List)
+from typing import (Optional, List, Tuple)
 
 from FaceDataServer.faceDetection import (faceCalibration, facemark)
 from FaceDataServer.Types import (RawFaceData,
                                  FaceDetectionError, Face, ExitCode,
-                                 FaceData)
+                                 FaceData, Cv2Image, Coord)
 from logging import getLogger, Logger
 import logging.config as loggingConfig
+import numpy as np
 
 # Loggers {{{
 configuu = {"version": 1
@@ -36,6 +37,40 @@ logger_servicer: Logger = getLogger('Servicer')
 # }}}
 
 
+def lines(img: Cv2Image, ends: List[Tuple[Coord, Coord]]):
+    if ends == []:
+        return img
+
+    def round_(a):
+        return tuple(map(round, a))
+    s, e = ends[0]
+    img_ = cv2.line(img, round_(s.toTuple()), round_(e.toTuple()), (0, 70, 0))
+    tail = ends[1:]
+    return lines(img_, tail)
+
+
+def face2Image(size, face: Face) -> Cv2Image:
+    h, w = size
+    blank_img = np.zeros((round(h), round(w), 3), np.uint8)
+    blank_img[:, :] = (60, 60, 60)
+    lineEnds = [(face.leftEye.bottom, face.leftEye.rightSide)
+               , (face.leftEye.rightSide, face.leftEye.top)
+               , (face.leftEye.top, face.leftEye.leftSide)
+               , (face.leftEye.leftSide, face.leftEye.bottom)
+               # right eye
+               , (face.rightEye.bottom, face.rightEye.rightSide)
+               , (face.rightEye.rightSide, face.rightEye.top)
+               , (face.rightEye.top, face.rightEye.leftSide)
+               , (face.rightEye.leftSide, face.rightEye.bottom)
+               # mouth
+               , (face.mouth.bottom, face.mouth.rightSide)
+               , (face.mouth.rightSide, face.mouth.top)
+               , (face.mouth.top, face.mouth.leftSide)
+               , (face.mouth.leftSide, face.mouth.bottom)
+                ]
+    lines(blank_img, lineEnds)
+
+
 def main():
     # Server setting
     server_address = "0.0.0.0"
@@ -46,6 +81,9 @@ def main():
     cap: cv2.VideoCapture = cv2.VideoCapture(0)
     if not cap.isOpened():
         cap.release()
+    videoSize = (cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                , cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+                 )
 
     # ========== calibration ==========
     try:
@@ -86,10 +124,13 @@ def main():
 
                 sock.sendto(data.toBinary(), (multicast_group, server_port))
 
+                cv2.imshow(face2Image(videoSize, face))
+
     except KeyboardInterrupt:
         pass
     finally:
         cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
